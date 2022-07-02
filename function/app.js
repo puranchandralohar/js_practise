9cb4ef1e481af50f50a45d1e41f1b2941bac79af
puranchandralohar <clpuran95@gmail.com>
2022-07-02 15:10:33 +0530

    function

---
+++ function/app.js
@@ -0,0 +1,53 @@
+// print message 10 times and count lines
+
+// function getMessage(msg,times,count){
+//     for(let i =1;i<=times;i++){
+//         console.log(msg,count);
+//         count++;
+//     }
+// }
+// getMessage("Hello!",20,1);
+
+// print 1 to n;
+
+// function showNumbers(n){
+//     for(let i=1;i<=n;i++){
+//         console.log(i);
+//     }
+// }
+// showNumbers(20);
+
+// print n to 1
+
+// function showNumbers(n){
+//     for(i=n;i>=1;i--){
+//         console.log(i);
+//     }
+// }
+// showNumbers(20)
+
+// print even digits upto n
+
+// function getEven(n,count){
+//     for(let i=1; i<=n;i++){
+//         if(i%2==0){
+//             console.log(i);
+//             // count++
+//         }      
+//     }
+//     // return count
+// }
+// getEven(20,0)
+
+// print sum of even digits
+
+// function sumOfEven(n1,n2,sum){
+//     for(i=n1;i<=n2;i++){
+//         if(i%2==0){
+//             sum = sum +i;
+//         }
+//     }
+//     return sum;
+// }
+// let s = sumOfEven(10,20,0);
+// console.log(s);
