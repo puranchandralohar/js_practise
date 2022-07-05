5f9dea53b5075d8e12620822b91bf73356820824
puranchandralohar <clpuran95@gmail.com>
2022-07-05 17:01:23 +0530

    array

--- array/questions.js
+++ array/questions.js
@@ -1,11 +1,20 @@
 // Return the first Element in an array
 
-    let arr = [
-        'Kolkata',
-        'Mumbai',
-        'Hydrabad',
-        'Chennei',
-        'Banglore'
-    ];
+    // let arr = [
+    //     'Kolkata',
+    //     'Mumbai',
+    //     'Hydrabad',
+    //     'Chennei',
+    //     'Banglore'
+    // ];
 
-    console.log(arr[0]);
+    // console.log(arr[0]);
+
+
+    function getFirst(arr){
+        return arr[0];
+
+    }
+    let first = getFirst(10,20,30,40,50);
+    console.log(first);
+
