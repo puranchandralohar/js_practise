a933c001c82eb5daee089a69983c203536527601
puranchandralohar <clpuran95@gmail.com>
2022-07-06 23:14:16 +0530

    changes made

---
+++ callback/app.js
@@ -0,0 +1,17 @@
+function calculator(message,callback){
+    console.log(message);
+    console.log(callback());
+}
+
+function add(n1,n2){
+    let sum = n1 + n2;
+    return sum;
+}
+
+function sub(n1,n2){
+    let min = n1 - n2;
+    return min;
+}
+
+calculator("Addition is ",()=>add(4,5));
+calculator("subtraction",sub)
