fc7dfe9936565859669bde93f70f8fbec5ba5438
puranchandralohar <clpuran95@gmail.com>
2022-07-08 17:54:19 +0530

    array questions added

--- array/questions.js
+++ array/questions.js
@@ -13,10 +13,38 @@
  /*************************************************************/   
 // Return the first Element in an array
 
-    function getFirst(arr){
-        return arr[0];
+    // function getFirst(arr){
+    //     return arr[0];
 
+    // }
+    // let first = getFirst([10,20,5,9]);
+    // console.log(first);
+
+/****************************************************************/
+
+// Return an array into an string
+
+// function getString(arr){
+//     let newstr="";
+//     for(let i=0;i<arr.length;i++){
+//         newstr+=arr[i]+" ";
+//     }
+//     console.log(newstr);
+
+// }
+// getString(["Eat","Sleep","Code","Repeat"]);
+
+/******************************************************************/
+
+//Reverse an Array
+
+function revArray(arr){
+    let rev=[];
+    for(let i=arr.length-1; i>=0;i--){
+        rev=rev+arr[i];
     }
-    let first = getFirst([10,20,5,9]);
-    console.log(first);
-
+    console.log(rev);
+    console.log(typeof rev);
+    
+}
+revArray(["Kapil","Ganguly","Mahi"]);
