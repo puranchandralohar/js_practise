f06c3cb3e69635d779c80b6b54c7f9b7d2b339c3
puranchandralohar <clpuran95@gmail.com>
2022-07-08 23:59:22 +0530

    Changes

--- array/questions.js
+++ array/questions.js
@@ -27,7 +27,11 @@
 // function getString(arr){
 //     let newstr="";
 //     for(let i=0;i<arr.length;i++){
-//         newstr+=arr[i]+" ";
+//         if(i==arr.length-1) newstr+=arr[i];
+//         else{
+//             newstr+=arr[i]+",";
+//         }
+       
 //     }
 //     console.log(newstr);
 
@@ -38,13 +42,13 @@
 
 //Reverse an Array
 
-function revArray(arr){
-    let rev=[];
-    for(let i=arr.length-1; i>=0;i--){
-        rev=rev+arr[i];
-    }
-    console.log(rev);
-    console.log(typeof rev);
-    
-}
-revArray(["Kapil","Ganguly","Mahi"]);
+// function revArray(arr){
+//     let rev=[];
+//     for(let i=arr.length-1; i>=0;i--){
+//         rev.push(arr[i]);
+//     }
+//     console.log(rev);
+//     console.log(typeof rev);
+
+// }
+// revArray(["Kapil","Ganguly","Mahi"]);
