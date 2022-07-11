61a32658722369b77c03ccc30e59d573473e3a33
puranchandralohar <clpuran95@gmail.com>
2022-07-11 18:00:42 +0530

    changes

---
+++ array/practise.js
@@ -0,0 +1,45 @@
+//Reverse
+
+// const arr = [1,2,3,4]
+// arr.reverse();
+// console.log(arr);
+
+//push
+// arr.push(5,6,7,8,9);
+
+//unshift
+// arr.unshift(0);
+// console.log(arr);
+
+//Include
+
+// const arr1 = [1,2,3,4,5]
+
+// console.log(arr1.includes(5)); //true
+// console.log(arr1.includes(10)); //false
+
+// console.log(arr1.includes(2,1)); //true
+// console.log(arr1.includes(2,2)); //false
+
+//isArray
+// console.log(Array.isArray(arr1));
+
+//from
+
+// let newArray = Array.from(arr1);
+// newArray[2]=10;
+
+// console.log(arr1);
+// console.log(newArray);
+
+//indexof
+let names = ["Aman", "Sohan", "Ronit", "Ram","Krishna"];
+
+console.log(names.indexOf("Ram"))
+
+
+
+
+
+
+
