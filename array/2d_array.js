feb051c856deb4a3c4cf28f49fcc47e10033430a
puranchandralohar <clpuran95@gmail.com>
2022-07-12 22:30:02 +0530

    changes

--- array/2d_array.js
+++ array/2d_array.js
@@ -1,9 +1,8 @@
-let arr = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [0]
-]
+// let arr = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
 
 // console.log(arr[0]);
 
@@ -33,11 +32,54 @@ let arr = [
 
 //Sum of all the elements in an array;
 
-let sum = 0;
+// let sum = 0;
 
-for(let i = 0; i<arr.length; i++){
-    for(j = 0; j<arr[i].length; j++){
-        sum+= arr[i][j]
-    }
-}
-console.log(sum);
+// for(let i = 0; i<arr.length; i++){
+//     for(j = 0; j<arr[i].length; j++){
+//         sum+= arr[i][j]
+//     }
+// }
+// console.log(sum);
+
+//---->
+// let rows = arr.length;
+// let start =0;
+// for(let i = 0; i<rows; i++,start++){
+//         sum+= arr[start][i]
+
+// }
+// console.log(sum);
+
+// let rows = arr.length;
+// let start =arr[0].length - 1;
+// for(let i = 0; i<rows; i++){
+//         sum+= arr[i][start];
+//         start-=1
+
+
+// }
+// console.log(sum);
+
+// //printing 1,2,3,4,8,7,6,5,9,10,11,12,16,15,14,13
+// let arr1 = [
+//     [1,2,3,4],
+//     [5,6,7,8],
+//     [9,10,11,12],
+//     [13,14,15,16]
+// ]
+
+// let rows = arr1.length;
+// let cols = arr1[0].length;
+
+// for(let i = 0; i<rows;i++){
+//     if(i%2==0){
+//         for(let j=0;j<cols;j++){
+//             console.log(arr1[i][j]);
+//         }
+//     }
+//     else {
+//         for(let j=cols-1; j>=0; j--){
+//             console.log(arr1[i][j]);
+//     }
+// }
+// }
