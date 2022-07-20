a0ea793bd9ecc7bb63b206408d2c28ef3da7b63c
puranchandralohar <clpuran95@gmail.com>
2022-07-20 23:47:44 +0530

    changes added

---
+++ string/questions.js
@@ -0,0 +1,17 @@
+// Add two strings
+
+let str1 = 'JavaScript';
+let str2 = 'is fun';
+
+console.log(`${str1} ${str2} and easy to learn.`)
+
+//>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
+
+// Slice 
+
+let str3 = 'JavaScript';
+
+console.log(str3.slice(0,4)); //Java
+
+//>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<\\
+
