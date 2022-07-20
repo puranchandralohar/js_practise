9266458d9d8b20b82ef1f6664b01421165dd7653
puranchandralohar <clpuran95@gmail.com>
2022-07-20 23:38:12 +0530

    Added

---
+++ objects/index.js
@@ -0,0 +1,10 @@
+let studDetails = {
+    name:'Aman',
+    age:'25',
+    address:'Kolkata',
+    role:'Front-End Dev'
+};
+
+// let name = studDetails['name'];
+let name = studDetails.name;
+console.log(name);
