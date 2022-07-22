2c0c36aefa1d6cf83c6b7898cf0cae63b1892497
puranchandralohar <clpuran95@gmail.com>
2022-07-22 23:57:13 +0530

    notes

--- z_notes/index.js
+++ z_notes/index.js
@@ -1,3 +1,9 @@
 // What is  JavaScript ?
 /* JavaScript is a programming language used to devlop interactive
-and dynamic web pages.*/
+and dynamic web pages.
+
+    JavaScript codes are written inside <script></script> tag.
+    Script tag can be written inside head tag or before the html 
+    closing tags. but the best way to write it is before html closing
+    tag. So that Javascript codes rendred after successfully loaded
+    the page. */
