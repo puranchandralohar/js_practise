05592d0b72b7a244f19cae581f351a40c01dcf55
puranchandralohar <clpuran95@gmail.com>
2022-07-30 13:15:13 +0530

    changes made

---
+++ DOM/Assignment1/readme.md
@@ -0,0 +1,14 @@
+
+<h3>About this Assignment</h3>
+<p>This Assignment is done by using JavaScript DOM. Some of JS Commands mentions below..</p>
+<ul>
+    <li>querySelectors</li>
+    <li>getElementsByTagName</li>
+    <li>getElementById</li>
+    <li>innerText / innerHTML</li>
+    <li>style</li>
+</ul>
+
+<h4>Preview</h4>
+<img src = "./js practice/DOM/Assignment1/images/output.png">
+
--- DOM/Assignment1/script.js
+++ DOM/Assignment1/script.js
@@ -1,5 +1,6 @@
 let changeName = document.getElementsByClassName("class1")[0];
 changeName.innerText = "Puran Chandra Lohar";
+changeName.style.fontWeight = "bold";
 
 let addPara = document.querySelector(".class2");
 addPara.innerHTML = "<p>Hey, I'm Puran Chandra Lohar, from AirCampus. I am a Aspiring Full Stack Developer </p>";
@@ -11,6 +12,7 @@ let selectedDivs = document.querySelectorAll(".divs");
 
 for(let i =0 ; i < selectedDivs.length; i++){
     selectedDivs[i].style.border = "1px solid black";
+    selectedDivs[i].style.margin = "5px 0";
     if(i==0){
         selectedDivs[i].innerText = "Forth";
     }
@@ -35,7 +37,7 @@ ninthDiv.innerHTML="<h3>My Experience with AirCampus</h3>";
 ninthDiv.style.color="red";
 ninthDiv.style.fontSize="32px";
 ninthDiv.style.textDecoration="underline";
+ninthDiv.style.backgroundColor="#dde4e6";
 
-let myExp = document.getElementsByTagName("p")[0];
-console.log(myExp)
-myExp.innerText = "AirCampus"
+let myExp = document.getElementsByTagName("p")[2];
+myExp.innerText = "First of All I would like to say thank you AirCampus for beleiving in me and giving me this opportunity. This is an Eye-Opening for me. My Experience with AirCampus till now is awesome. I have learnt so many things. Mentors are really amazing. Their teaching style and friendly nature is really great."
