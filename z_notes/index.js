6dc5dcd5638b73608ce6d93c23b89521a1e82656
puranchandralohar <clpuran95@gmail.com>
2022-07-31 13:06:54 +0530

    added

--- z_notes/index.js
+++ z_notes/index.js
@@ -7,3 +7,75 @@ and dynamic web pages.
     closing tags. but the best way to write it is before html closing
     tag. So that Javascript codes rendred after successfully loaded
     the page. */
+    
+//
+
+//What is Hoisting ?
+/** It is phenomenon where you can access the variables and functions 
+  before its decleration. 
+    Since JS allocate a memory for variables in the start of the code.
+    by using var ,it will be undefined .
+*/
+    // example
+
+    // console.log(a);
+    // fun()
+
+    // var a = 10 ;
+    // function fun(){
+    //     console.log("JS is Fun")
+    // }
+
+//Diffrence between for-in and for-of loop
+
+// for of - it is used to itreate through values. Used for Array and strings.
+
+//for in- it is used to itreate through keys / indexs. used for objects.
+
+// examples
+//in Object--
+
+// let phone = {
+//     model:1110,
+//     company:"Nokia",
+//     battery:"2000mh",
+//     network:"2G"
+// }
+
+// for(let keys in phone){
+//     console.log(phone[keys]);
+// }
+
+
+//in Arrays - 
+
+// let friends  = ["Satya","Hema","Anish","Gayatri"]
+
+
+// for(let keys in friends){
+//     console.log(keys);
+// }
+//0,1,2,3
+
+// for(let key of friends){
+//     console.log(key);
+// }
+//Satya, Hema , Anish , Gayatri
+
+// Flatten-Array
+
+// function flateddArray(arr){
+//     let fltenArr = [];
+//     for(let i =0 ; i<arr.length;i++){
+//         if(Array.isArray(arr[i])){
+//             fltenArr = [...fltenArr,...flateddArray(arr[i])];
+//         }
+//         else{
+//             fltenArr.push(arr[i])
+//         }
+//     }
+//     return fltenArr;
+// }
+
+// let arr = [[1,2,3,4],[],[5,6],["A","B"]]
+// console.log(flateddArray(arr));
