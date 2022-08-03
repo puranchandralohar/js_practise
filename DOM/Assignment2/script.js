edeac7bab7688be913d163ae662e0e7f1de8e849
puranchandralohar <clpuran95@gmail.com>
2022-08-03 18:25:14 +0530

    added

--- DOM/Assignment2/script.js
+++ DOM/Assignment2/script.js
@@ -1,16 +1,45 @@
+let form = document.querySelector("form");
 
-let userName = document.getElementsByClassName("user_name");
-let email = document.getElementsByClassName("user_email");
-let phone = document.getElementsByClassName("user_phone");
+let userName = document.querySelector(".user_name");
+let email = document.querySelector(".user_email");
 
-let button = document.getElementsByClassName("btn");
-// console.log(button);
+let phone = document.querySelector(".user_phone");
 
-// button.addEventListener("click",()=>{alert("hello")});
-button.addEventListener('click',errMsg);
-   
-function errMsg(){
-    if(userName.value =='' || email.value=="" || phone.value ==""){
-        alert("Empty Fields");
-    }
+let button = document.querySelector(".btn");
+
+let msgBox = document.querySelector(".alertMsg");
+
+let tbody = document.querySelector("tbody");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!userName.value || !email.value || !phone.value) {
+    msgBox.innerText = "Empty Input Field(s)";
+    msgBox.style.color="red";
+    setTimeout(() => {
+      msgBox.innerText = "";
+    }, 2000);
+  } else {
+    msgBox.innerText = "Data Inserted Successfully!!";
+    msgBox.style.color = "green";
+    setTimeout(() => {
+      msgBox.innerText = "";
+    }, 2000);
+
+    tbody.innerHTML += `<tr>
+    <td>${userName.value}</td>
+    <td>${email.value}</td>
+    <td>${phone.value}</td>
+    <td><input type="button" id="rm-btn" value="Remove" onclick="removeData()"></td></tr>`;
+
+  }
+});
+
+
+function removeData(){
+  document.querySelector("#rm-btn").closest("tr").remove();
+  setTimeout(() => {
+    msgBox.innerText = "Item deleted successfully";
+    msgBox.style.color = "red";
+  }, 2000);
 }
