4c9fd0f51aa1e93c1754a648ab404c945b533295
puranchandralohar <clpuran95@gmail.com>
2022-08-05 12:40:10 +0530

    changes

--- DOM/Assignment3/script.js
+++ DOM/Assignment3/script.js
@@ -5,7 +5,6 @@ let buttons = Array.from(document.querySelectorAll(".btn"));
 buttons.map((btn)=>{
     btn.addEventListener('click',(event)=>{
         let key = event.target.innerText;
-        // console.log(key);
         
         if(key =='C'){
             display.innerText="";
@@ -19,7 +18,41 @@ buttons.map((btn)=>{
         }else if(key=="x²"){
             display.innerText = display.innerText ** 2;
 
+        }else if(key=="π"){
+            display.innerText = Math.PI*(display.innerText);
+
+        }else if(key=="%"){
+            display.innerText = (display.innerText)/100;
+
+        }else if(key=="sin"){
+            display.innerText = Math.sin(display.innerText);
+
+        }else if(key=="cos"){
+            display.innerText = Math.cos(display.innerText);
+
+        }else if(key=="tan"){
+            display.innerText = Math.tan(display.innerText);
+
+        }else if(key=="log"){
+            display.innerText = Math.log(display.innerText);
+
+        }else if(key=="x!"){
+            
+            let fact = 1;
+            for(let  i = display.innerText; i>0 ; i--){
+                fact = fact*i;
+            }
+            display.innerText = fact;
+
+        }else if(key=="±"){
+            display.innerText = -1*(display.innerText);
+
+        }else if(key=="x^"){
+            let j =display.innerText;
+            display.innerText = display.innerText ** j;
+
         }
+        
         else if(key=='='){
             display.innerText = eval(display.innerText);
         }
