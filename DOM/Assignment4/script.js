ff67e2d5853de7fbf735e558d659dfd0f4eb9c58
puranchandralohar <clpuran95@gmail.com>
2022-08-07 18:41:49 +0530

    changes made

--- DOM/Assignment4/script.js
+++ DOM/Assignment4/script.js
@@ -5,25 +5,55 @@ form.addEventListener('submit',additem);
 function additem(e){
     e.preventDefault();
     let item = document.getElementById("item");
+    
     if(item.value.trim().length > 1){
-        let listItem  = document.querySelector('.todolist');
-        let div = document.createElement('div');
-        div.classList.add('list');
+        let list  = document.querySelector('.list');
 
-        let todo = document.createElement('input');
+        let listItem = document.createElement('li');
+        listItem.classList.add('list-item');
 
-        todo.type = 'checkbox';
-        todo.name='mylist';
-        todo.id='checkbox';
+
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        input.name='check';
+        input.id='check';
 
         let label = document.createElement('label');
-        label.id = 'label'
-        label.htmlFor = "checkbox";
+
+        let editBtn = document.createElement('button');
+        editBtn.classList.add('btn')
+        editBtn.id = "edit"
+        editBtn.innerText = 'Edit';
+
+        let delBtn = document.createElement('button');
+        delBtn.classList.add('btn')
+        delBtn.innerText = 'Delete'
+        delBtn.id = 'del';
 
         label.innerText = item.value;
-        div.appendChild(todo);
-        div.appendChild(label);
-        listItem.append(div);
-    }
+        listItem.appendChild(input);
+        listItem.appendChild(label);
+        listItem.appendChild(editBtn);
+        listItem.appendChild(delBtn);
 
+        list.append(listItem);
+
+        editBtn.addEventListener("click", (e) => {
+            if (editBtn.innerText == "Edit") {
+              editBtn.innerText = "Save";
+              label.contentEditable = true;
+              label.style.border = "1px solid black";
+            } else {
+              editBtn.innerText = "Edit";
+              label.contentEditable =false;
+              label.style.border = "";
+
+            }
+          });
+
+        delBtn.addEventListener('click',(e)=>{
+            e.target.parentElement.parentElement.removeChild(listItem);
+        });
+
+    }
 }
