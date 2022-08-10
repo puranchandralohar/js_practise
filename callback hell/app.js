b5a5780c68b6d5e14907bc1f1f867c551bb56651
puranchandralohar <clpuran95@gmail.com>
2022-08-10 21:57:31 +0530

    promise scenario2

--- callback hell/app.js
+++ callback hell/app.js
@@ -48,23 +48,77 @@
 // paying the bill,
 // leaving the hotel
 
-function prepration(callback){
-    setTimeout(()=>{
-        console.log('preparing food');
-        console.log('server deliver food to the table');
-        callback()
-    },3000)
+// function prepration(callback){
+//     setTimeout(()=>{
+//         console.log('preparing food');
+//         console.log('server deliver food to the table');
+//         callback()
+//     },3000)
 
+// }
+
+// function eatingFood(){
+//     setTimeout(()=>{
+//         console.log('Eating my meal');
+//         console.log('Paying Bills');
+//         console.log('Leaving Hotel');
+//     },2000);
+
+// }
+
+
+// function dinnerScenario(){
+
+//     console.log('entering into the hotel');
+//     console.log('Ordering Masala Dosa');
+//     console.log('server informing the chef to prepre food');
+//     prepration(eatingFood);
+// }
+
+// dinnerScenario();
+
+/////////////////////////////////////////////////////////
+
+// Promise
+
+
+function prepration(){
+    return new Promise((resolve,reject)=>{
+    let vegetablesPresent = true;
+    if(vegetablesPresent){
+            setTimeout(()=>{
+                console.log('preparing food');
+                console.log('Server delevring the food to table');
+                resolve();
+            },3000);
+        } else{
+            reject('No vegetables');
+        }
+    });
 }
 
 function eatingFood(){
-    setTimeout(()=>{
-        console.log('Eating my meal');
-        console.log('Paying Bills');
-        console.log('Leaving Hotel');
-    },2000);
-
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Eating food');
+            resolve();
+        },2000);
+    });
 }
+function payment(){
+    return new Promise((resolve,reject)=>{
+        let pursePresent = true;
+        if(pursePresent){
+            setTimeout(()=>{
+                console.log('paying the bills');
+                console.log('leaving the hotel');
+                resolve();
+            });
+        }else{
+            reject('Run from hotel');
+        }
+    })
+} 
 
 
 function dinnerScenario(){
@@ -72,7 +126,18 @@ function dinnerScenario(){
     console.log('entering into the hotel');
     console.log('Ordering Masala Dosa');
     console.log('server informing the chef to prepre food');
-    prepration(eatingFood);
+    prepration()
+    .then(()=>{
+        console.log('food preparing and delievering => Successfully');
+        return eatingFood();
+    })
+    .then(()=>{
+        console.log('Successfully');
+        return payment();
+    })
+    .then(()=>console.log('sucessfull'))
+    .catch(()=>console.log('failed'));
+    
 }
 
 dinnerScenario();
