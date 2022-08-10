8af8b27290f73b9647e5d475239e555332feea92
puranchandralohar <clpuran95@gmail.com>
2022-08-10 19:29:27 +0530

    promises example added

--- callback hell/app.js
+++ callback hell/app.js
@@ -1,25 +1,39 @@
 
-function hotelVisit(){
+// function hotelVisit(){
 
-    console.log('Going to the Hotel');
-    console.log('order food => 2 Roties with Daal');
-    console.log('Food is getting prepared');
-    preparedFood(eatRoties);
-}
+//     console.log('Going to the Hotel');
+//     console.log('order food => 2 Roties with Daal');
+//     console.log('Food is getting prepared');
+//     preparedFood(eatRoties);
+// }
 
-function preparedFood(starteating){
+// function preparedFood(starteating){
+//     setTimeout(()=>{
+//         console.log('food is prepared');
+//         console.log('Deleiverd to the Table');
+//         starteating();
+//     },10000)
+// }
+
+// function eatRoties() {
+//     setTimeout(()=>{
+//         console.log('Eating Food');
+//         console.log('Pay Bills');
+//     },5000)
+// }
+
+// hotelVisit();
+
+
+//Promises
+
+let momsPromise = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        console.log('food is prepared');
-        console.log('Deleiverd to the Table');
-        starteating();
-    },10000)
-}
-
-function eatRoties() {
-    setTimeout(()=>{
-        console.log('Eating Food');
-        console.log('Pay Bills');
+        resolve('collected all golds');
+        reject('killed by adheera')
     },5000)
-}
+});
 
-hotelVisit();
+momsPromise
+    .then((result) => console.log('Climax',result))
+    .catch((error)=>console.log(error));
