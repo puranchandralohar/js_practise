50f88774f9f1ba073f90793db4b5cd54643bed45
puranchandralohar <clpuran95@gmail.com>
2022-10-03 18:32:19 +0530

    Added

---
+++ practise/String.js
@@ -0,0 +1,12 @@
+// Given two string arrays word1 and word2, 
+// return true if the two arrays represent the same string, 
+// and false otherwise.
+
+function isSameString(word1,word2){
+
+    return word1.join("") === word2.join("");
+}
+
+let result = isSameString(["ab","cd"],["a","bcd"]);
+console.log(result)
+
