d40814ca399a607aa2bff772f822b6c168200151
puranchandralohar <clpuran95@gmail.com>
2022-10-03 18:40:48 +0530

    Added

--- practise/String.js
+++ practise/String.js
@@ -10,3 +10,15 @@ function isSameString(word1,word2){
 let result = isSameString(["ab","cd"],["a","bcd"]);
 console.log(result)
 
+// ************************************************
+
+// Given a string s, 
+// return the string after replacing 
+// every uppercase letter with the same lowercase letter.
+
+function toLowerCase(s){
+    return s.toLowerCase();
+}
+
+console.log(toLowerCase('HELLO'));
+
