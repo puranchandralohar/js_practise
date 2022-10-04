f8dda4c7a707a3ede7a2d2c8f222f48e6bb6f1b8
puranchandralohar <clpuran95@gmail.com>
2022-10-04 15:55:40 +0530

    added

--- practise/Array.js
+++ practise/Array.js
@@ -20,3 +20,27 @@
 // };
 
 // console.log(twoSum([1,2,5,4,7,5],6));
+// ********************************************************
+
+// Given an array nums with n objects colored red, 
+// white, or blue, sort them in-place so that objects 
+// of the same color are adjacent, with the colors in 
+// the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+// var sortColors = function(nums) {
+//     let temp;
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[i]>=nums[j]){
+//             temp=nums[i];
+//             nums[i]=nums[j];
+//             nums[j]=temp;
+//         } 
+//         }
+       
+//     }
+//     return nums;
+// };
+// console.log(sortColors([2,0,2,1,1,0]))
