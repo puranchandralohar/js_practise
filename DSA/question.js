cad704caa09f9abec3b0922dde4ded295cd4b21f
puranchandralohar <clpuran95@gmail.com>
2022-10-14 20:35:55 +0530

    Binary Search Question

---
+++ DSA/question.js
@@ -0,0 +1,14 @@
+// [1,2,3,4] is input array
+// Expected output [24, 12, 8, 6]
+// Explanation:
+// Product of all elements except current element
+// [2*3*4, 1*3*4, 1*2*4, 1*2*3]
+
+const getProducts = (arr)=>{
+  let temp=1; let arr1=[];
+  
+  return arr1;
+
+}
+
+console.log(getProducts([1,2,3,4]));
