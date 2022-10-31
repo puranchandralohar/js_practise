215f68d5ed9a869821c983b171aa862aea6d56c1
puranchandralohar <clpuran95@gmail.com>
2022-10-31 23:27:04 +0530

    Added

--- Closure/app.js
+++ Closure/app.js
@@ -1,12 +1,40 @@
-function add(){
- let num1=5;
- 
- function sub(){
-    let num2=4;
-    console.log(num1+num2)
- }
-sub()
+// function add(){
+//  let num1=5;
 
-}
+//    function sub(num2){
+//       console.log(num1+num2)
+//    }
+//    sub(4)
 
-add()
+// }
+
+// let x = add()
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// function a(x){ 
+//     function b(){
+//         function c(){
+//             function d(){
+//                 const  { name,role } = x;
+//                 console.log(name);
+//                 function e(){
+//                     console.log(role)
+//                 }
+//                 e()
+//             }
+//             d()
+//         }
+//         c()
+//     }
+//     b()
+// }
+
+// a({
+//     name:"Puran",
+//     role:"WebDevloper"
+// });
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
---
+++ Closure/interviewqs.js
@@ -0,0 +1,45 @@
+// Closure
+
+
+// function x(){
+//     var i=1;
+//     setTimeout(function(){
+//         console.log(i)
+//     },3000);
+//     console.log("Hello World");
+// };
+
+// x()
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// function x(){
+//     for(var i=1;i<=5;i++){
+//         setTimeout(function(){
+//             console.log(i)
+//         },i*1000)
+//     }
+
+// }
+
+// x()
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// function x(){
+    
+//     for(var i=1;i<=5;i++){
+//         function y(i){
+//             setTimeout(function(){
+//                 console.log(i)
+//             },i*1000)
+//         }
+//         y(i)
+//     }
+
+// }
+
+// x()
+
