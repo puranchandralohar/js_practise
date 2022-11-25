14b2675f0bc4d3fa83f0bb13ef2717c30879a9eb
puranchandralohar <clpuran95@gmail.com>
2022-11-25 22:34:16 +0530

    Word Count

---
+++ questions/wordCount.js
@@ -0,0 +1,12 @@
+// Count Word in a sentence?
+
+function countWord(sentence){
+
+    return sentence.split(" ").length;
+
+
+}
+
+let result  = countWord("India is a beautiful Country, Saare Jahan se Accha, Hindusta Hamara");
+
+console.log(result)
