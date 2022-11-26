3859943b24e31e19847d5914970678e74ef5a4f1
puranchandralohar <clpuran95@gmail.com>
2022-11-26 12:38:20 +0530

    added

--- promise/index.js
+++ promise/index.js
@@ -1,19 +1,42 @@
 // Promise
 // a promise object represent an eventual completion or failure of an async operation and its value.
 
-const myPromise = new Promise((resolve, reject) => {
-  let number = 1;
-  if (number > 2) {
-    resolve("Promise is resolved");
-  } else {
-    reject("Promise Rejected");
-  }
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   let number = 1;
+//   if (number > 2) {
+//     resolve("Promise is resolved");
+//   } else {
+//     reject("Promise Rejected");
+//   }
+// });
 
-myPromise
-  .then((returnedValue) => {
-    console.log(returnedValue);
-  })
-  .catch((value) => {
-    console.log(value);
-  });
+// myPromise
+//   .then((returnedValue) => {
+//     console.log(returnedValue);
+//   })
+//   .catch((value) => {
+//     console.log(value);
+//   });
+
+
+
+// const hello = new Promise((resolve, reject) => {
+//     let n = 15;
+//     if(n<6){
+//         resolve(setTimeout(()=>{
+//             alert("Hello World")
+//         },2000)
+//        )
+//     }
+//     else{
+//         reject("Failed")
+//     } 
+//   });
+  
+//   hello
+//     .then(()=>{
+//        console.log("Alert Message") 
+//     })
+//     .catch(() => {
+//       console.log("Failed");
+//     });
