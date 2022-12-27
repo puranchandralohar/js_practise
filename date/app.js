b3c3344d0975a38febc6e4d3c06a7b09f1ba878b
puranchandralohar <clpuran95@gmail.com>
2022-12-27 12:59:13 +0530

    Date Methods

--- date/app.js
+++ date/app.js
@@ -1,4 +1,29 @@
 
-const getDate=()=>{
+const getDate =()=>{
     document.querySelector('.date').innerHTML = new Date()
 }
+
+
+
+//      Date Methods
+
+let date = new Date();
+
+console.log(date)
+console.log(date.toDateString())
+console.log(date.toLocaleDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleTimeString())
+
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+console.log(date.getSeconds())
+console.log(date.getMilliseconds())
+
+console.log(date.getDate())
+console.log(date.getDay())
+console.log(date.getMonth())
+console.log(date.getFullYear())
+console.log(date.toUTCString())
+
