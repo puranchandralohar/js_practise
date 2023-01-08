9abe27cccc7d809b19741faebe606a411955bc70
puranchandralohar <clpuran95@gmail.com>
2023-01-08 22:09:38 +0530

    promise practise

--- promise/index.js
+++ promise/index.js
@@ -40,3 +40,61 @@
 //     .catch(() => {
 //       console.log("Failed");
 //     });
+
+
+////////////////////////////////////////////////////////////////
+
+// console.log('start')
+
+// const promise1 = new Promise((resolve, reject) => {
+//   console.log(1)
+// })
+
+// console.log('end');
+
+//////////////////////////////////////////////////////
+
+// console.log('start');
+
+// const promise1 = new Promise((resolve, reject) => {
+//   console.log(1)
+//   resolve(2)
+// })
+
+// promise1.then(res => {
+//   console.log(res)
+// })
+
+// console.log('end');
+
+////////////////////////////////////
+
+// console.log('start');
+
+// const promise1 = new Promise((resolve, reject) => {
+//   console.log(1)
+//   resolve(2)
+//   console.log(3)
+// })
+
+// promise1.then(res => {
+//   console.log(res)
+// })
+
+// console.log('end');
+
+//output --  Start,1,3,end,2
+
+//////////////////////////////////////////////////////////////
+
+// console.log('start');
+
+// const promise1 = new Promise((resolve, reject) => {
+//   console.log(1)
+// })
+
+// promise1.then(res => {
+//   console.log(2)
+// })
+
+// console.log('end');
