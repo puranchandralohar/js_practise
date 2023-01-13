3ba06dd93d4bbfe217c4d69060f06a552d1f1b00
puranchandralohar <clpuran95@gmail.com>
2023-01-13 19:56:54 +0530

    added

--- polyfills/app.js
+++ polyfills/app.js
@@ -1,20 +1,69 @@
+// custom forEach
+// let arr = [5,4,3,2,1]
+
+
+
+// Array.prototype.forEach = null;
+
+
+// if(!Array.prototype.forEach){
+
+//     Array.prototype.forEach = function(callback){
+//         for(let val of this){
+//             callback(val)
+//         }
+//     }
+// }
+
+// arr.forEach((value)=>{
+//     console.log(value*2)
+// })
+
+
 let arr = [5,4,3,2,1]
 
 
 
-Array.prototype.forEach = null;
+// custom map
+
+// Array.prototype.map = null;
 
 
-if(!Array.prototype.forEach){
+// if(!Array.prototype.map){
 
-    Array.prototype.forEach = function(callback){
-        for(let val of this){
-            callback(val)
-        }
-    }
-}
+    // Array.prototype.mymap = function(callback){
+    //     let result =[];
+    //     for(let val of this){
+    //         let x = callback(val)
+    //         result.push(x)
+    //     }
+    //     return result
+    // }
+// }
 
-arr.forEach((value)=>{
-    console.log(value*2)
-})
+// arr.mymap((value)=>{
+//     console.log(value*4)
+// })
 
+
+
+// Custom Filter
+
+Array.prototype.myFilter = function(callback){
+ 
+    var arr = [];                         // step 1
+    for(let val of this)
+    {
+     if(callback(val) == true)        // step 2
+     {
+      arr.push(val);                  // step 3
+     }
+    } 
+    return arr;                           // step 4
+   }
+
+   let result = arr.myFilter((a)=>{
+    return a>4
+   })
+
+   console.log(result);
